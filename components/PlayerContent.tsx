df1350c1b8b8853852cd404b2054f6a4a6d9cdca
Michael <sitthisaklo@kkumail.com>
2023-10-16 20:41:45 +0700

    fixed-use-sound-module-error

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import useSound from "use-sound";
 import { useEffect, useState } from "react";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
-
 import { Song } from "@/types";
 import usePlayer from "@/hooks/usePlayer";
-
+import useSound from "use-sound";
 import LikeButton from "./LikeButton";
 import MediaItem from "./MediaItem";
 import Slider from "./Slider";
